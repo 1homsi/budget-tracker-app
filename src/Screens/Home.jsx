b6c8da47701dbc099ccccb96c89ab13e39ce9a76
Mohamad Homsi <homsimohamad4@gmail.com>
2024-03-25 15:20:52 +0200

    Update Home.jsx

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
 import Header from "../Components/Header";
 import useColors from "../../hooks/useColors";
 
 const Home = () => {
-  const { bgColor } = useColors();
+  const { bgColor, textColor } = useColors();
+  const [spentThisMonth, setSpentThisMonth] = useState(0);
+  const [balance, setBalance] = useState(0);
 
   return (
     <View
@@ -14,8 +16,65 @@ const Home = () => {
       }}
     >
       <Header />
+
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            color: textColor,
+          }}
+        >
+          Balance
+        </Text>
+        <Text
+          style={{
+            color: textColor,
+          }}
+        >
+          ${balance.toFixed(2)}
+        </Text>
+        <Text
+          style={{
+            color: textColor,
+          }}
+        >
+          Spent this Month
+        </Text>
+        <Text
+          style={{
+            color: textColor,
+          }}
+        >
+          ${spentThisMonth.toFixed(2)}
+        </Text>
+      </View>
+      <View
+        style={[
+          styles.separator,
+          {
+            backgroundColor: textColor,
+          },
+        ]}
+      ></View>
     </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+    alignSelf: "center",
+  },
+});
